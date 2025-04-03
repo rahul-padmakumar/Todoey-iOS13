eac210bfd2a10694c2521b46f7a09e233e1de78d
rahulp <rahulp5875@gmail.com>
2025-04-03 11:20:23 +0530

    Custom delete action

--- Todoey/ViewController.swift
+++ Todoey/ViewController.swift
@@ -88,35 +88,28 @@ class ViewController: UITableViewController {
         tableView.reloadData()
     }
     
-    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
-        return UITableViewCell.EditingStyle.delete
-    }
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
     
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete{
-            if let item = self.itemArray?[indexPath.row]{
-                do{
-                    try realm.write{
-                        realm.delete(item)
+        let deleteAction = UIContextualAction(
+            style: .destructive, title: nil) { _, _, completion in
+                
+                if let item = self.itemArray?[indexPath.row]{
+                    do{
+                        try self.realm.write{
+                            self.realm.delete(item)
+                            self.tableView.reloadData()
+                        }
+                    } catch {
+                        print("Delete error \(error)")
                     }
-                } catch {
-                    print("Delete error \(error)")
                 }
+                
+                completion(true)
             }
-            self.tableView.reloadData()
-        }
+        deleteAction.image = UIImage(systemName: "trash")
+        deleteAction.backgroundColor = .blue
+        return UISwipeActionsConfiguration(actions: [deleteAction])
     }
-    
-//    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-//    
-//        let deleteAction = UIContextualAction(
-//            style: .destructive, title: nil) { _, _, completion in
-//                completion(true)
-//            }
-//        deleteAction.image = UIImage(systemName: "trash")
-//        deleteAction.backgroundColor = .blue
-//        return UISwipeActionsConfiguration(actions: [deleteAction])
-//    }
     
     private func loadItems(){
         itemArray = selectedCategory?.items.sorted(byKeyPath: "dateCreated", ascending: true)
